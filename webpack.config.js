8ca916497c9e332fbe882576b1f7328b5c20ef6d
Tejas Pancholi <tejas.er@outlook.com>
2017-07-24 19:22:50 +0530

    setup webpack and bundle.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path'); // getting node path module loaded up
+// seting up config object
+const config = {
+  entry: path.join(__dirname, './client/index.js'), // absolute path to our index file
+  output: {  // our output configuration
+    path: path.join(__dirname, './public/'), // output path (directory/folder)
+    filename: 'bundle.js' //output bundled javascript file name
+  },
+  module: { //we define loaders here
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // check for .js or .jsx files using regex
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
+// export the config object
+module.exports = config; //this is es5 because webpack currently supports es5 only
